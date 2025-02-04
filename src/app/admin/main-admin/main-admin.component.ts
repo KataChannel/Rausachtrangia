62ab58faff81ef5c62998629b0492914322e1010
quocbao1983 <quocbao280783@gmail.com>
2025-02-05 03:11:25 +0700

    update

--- src/app/admin/main-admin/main-admin.component.ts
+++ src/app/admin/main-admin/main-admin.component.ts
@@ -29,7 +29,7 @@ export class MainAdminComponent implements OnInit {
   Menus: any[] = [
    // {id:3,pid:0,Title:"Demo" ,Slug:"demo"},
     {id:1,pid:0,Title:"Dashboard",Slug:"dashboard"},
-    {id:1,pid:0,Title:"Menu",Slug:"menu"},
+    // {id:1,pid:0,Title:"Menu",Slug:"menu"},
     {id:2,pid:0,Title:"Sản Phẩm" ,Slug:"sanpham"},
     // {id:3,pid:0,Title:"Danh Mục" ,Slug:"danhmuc"},
     {id:4,pid:0,Title:"Bài Viết" ,Slug:"baiviet",
@@ -46,21 +46,21 @@ export class MainAdminComponent implements OnInit {
     {id:3,pid:0,Title:"Đơn Hàng" ,Slug:"donhang"},
     // {id:3,pid:0,Title:"Giỏ Hàng" ,Slug:"giohang"},
     {id:3,pid:0,Title:"Slide" ,Slug:"slide"},
-    {id:3,pid:0,Title:"Module" ,Slug:"module"},
-    {id:3,pid:0,Title:"Page" ,Slug:"page"},
-    {id:4,pid:0,Title:"Xuất Nhập Tồn" ,Slug:"xnt",
-    children:[
-      // {id:1,pid:4,Title:"Xuất Kho" ,Slug:"xuatkho"},
-      // {id:1,pid:4,Title:"Nhập Kho" ,Slug:"nhapkho"},
-      {id:1,pid:4,Title:"Tồn Kho" ,Slug:"tonkho"},
-    ]
-    },
+    //{id:3,pid:0,Title:"Module" ,Slug:"module"},
+   // {id:3,pid:0,Title:"Page" ,Slug:"page"},
+    // {id:4,pid:0,Title:"Xuất Nhập Tồn" ,Slug:"xnt",
+    // children:[
+    //   // {id:1,pid:4,Title:"Xuất Kho" ,Slug:"xuatkho"},
+    //   // {id:1,pid:4,Title:"Nhập Kho" ,Slug:"nhapkho"},
+    //   {id:1,pid:4,Title:"Tồn Kho" ,Slug:"tonkho"},
+    // ]
+    // },
     {id:3,pid:0,Title:"Khách Hàng" ,Slug:"khachhang"},
     {id:3,pid:0,Title:"Liên Hệ" ,Slug:"lienhe"},
     {id:3,pid:0,Title:"Chương Trình Khuyến Mãi" ,Slug:"khuyenmai"},
-    {id:3,pid:0,Title:"User" ,Slug:"user"},
-    {id:3,pid:0,Title:"Usergroup" ,Slug:"usergroup"},
-    {id:3,pid:0,Title:"Cấu hình" ,Slug:"cauhinh"},
+    //{id:3,pid:0,Title:"User" ,Slug:"user"},
+    //{id:3,pid:0,Title:"Usergroup" ,Slug:"usergroup"},
+    //{id:3,pid:0,Title:"Cấu hình" ,Slug:"cauhinh"},
   ]
   Today:any=new Date()
   
