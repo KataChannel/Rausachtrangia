aa11b46018f8f824e92183eb10f746b9a43f4d48
chikiet88 <chikiet88@gmail.com>
2025-02-11 09:55:42 +0700

    update

--- src/app/admin/khachhang/listkhachhang/listkhachhang.component.ts
+++ src/app/admin/khachhang/listkhachhang/listkhachhang.component.ts
@@ -20,7 +20,11 @@ import { KhachhangsService } from './listkhachhang.service';
 import { MatSelectModule } from '@angular/material/select';
 import { CommonModule } from '@angular/common';
 import { BanggiasService } from '../../banggia/listbanggia/listbanggia.service';
-
+import { DonhangsService } from '../../donhang/listdonhang/listdonhang.service';
+import { ConvertDriveData } from '../../../shared/shared.utils';
+import moment from 'moment';
+import * as XLSX from 'xlsx';
+import { MatSnackBar } from '@angular/material/snack-bar';
 @Component({
   selector: 'app-listkhachhang',
   templateUrl: './listkhachhang.component.html',
@@ -80,17 +84,19 @@ export class ListkhachhangComponent {
   Columns: any[] = [];
   ListKhachhang: any[] = ListKhachhang;
   ListBanggia: any[] = [];
-
+  CountItem: number =0;
   @ViewChild(MatPaginator) paginator!: MatPaginator;
   @ViewChild(MatSort) sort!: MatSort;
   @ViewChild('drawer', { static: true }) drawer!: MatDrawer;
 
   private _KhachhangsService: KhachhangsService = inject(KhachhangsService);
   private _BanggiasService: BanggiasService = inject(BanggiasService);
+  private _DonhangsService: DonhangsService = inject(DonhangsService);
 
   constructor(
     private _breakpointObserver: BreakpointObserver,
-    private _router: Router
+    private _router: Router,
+    private _snackBar: MatSnackBar
   ) {}
 
   async ngOnInit(): Promise<void> {
@@ -200,12 +206,96 @@ export class ListkhachhangComponent {
 
   create(): void {
     this.drawer.open();
-    this._router.navigate(['admin/khachhangs', 0]);
+    this._router.navigate(['admin/khachhang', 0]);
   }
 
   goToDetail(item: any): void {
     this.drawer.open();
     this.Detail = item;
-    this._router.navigate(['admin/khachhangs', item.id]);
+    this._router.navigate(['admin/khachhang', item.id]);
   }
+
+      async LoadDrive()
+        {
+              const DriveInfo ={
+                IdSheet:'15npo25qyH5FmfcEjl1uyqqyFMS_vdFnmxM_kt0KYmZk',
+                SheetName:'Khachhangimport',
+                ApiKey:'AIzaSyD33kgZJKdFpv1JrKHacjCQccL_O0a2Eao'
+              }
+            const result:any =  await this._DonhangsService.getDrive(DriveInfo) 
+            const data =  ConvertDriveData(result.values);  
+            console.log(data);
+            const updatePromises = data.map(async (v:any) => {
+              const item = this._KhachhangsService.ListKhachhang().find((v1) => v1.MaKH === v.MaKH);
+              if (item) {
+              const item1 = {...item, ...v}
+              console.log(item1);
+              
+              //await this._KhachhangsService.updateOneKhachhang(item1);
+              }
+            });
+            Promise.all(updatePromises).then(() => {
+              this._snackBar.open('Cập Nhật Thành Công', '', {
+              duration: 1000,
+              horizontalPosition: "end",
+              verticalPosition: "top",
+              panelClass: ['snackbar-success'],
+              });
+            //  window.location.reload();
+            });
+      }
+  
+    readExcelFile(event: any) {
+      const file = event.target.files[0];
+      const fileReader = new FileReader();
+      fileReader.onload = (e) => {
+        const data = new Uint8Array((e.target as any).result);
+        const workbook = XLSX.read(data, { type: 'array' });
+        const sheetName = workbook.SheetNames[0];
+        const worksheet = workbook.Sheets[sheetName];
+        const jsonData = XLSX.utils.sheet_to_json(worksheet, { raw: true });
+        console.log(jsonData);
+        // const transformedData = jsonData.map((v: any) => ({
+        //   Title: v.Title.trim(),
+        //   MaSP: v.MaSP.trim(),
+        //   giagoc: Number(v.giagoc),
+        //   dvt: v.dvt,
+        // }));
+      const updatePromises = jsonData.map(async (v:any) => {
+        const item = this._KhachhangsService.ListKhachhang().find((v1) => v1.MaKH === v.MaKH);
+        if (item) {
+        const item1 = {...item, ...v}
+        //await this._KhachhangsService.updateOneKhachhang(item1);
+        }
+      });
+      Promise.all(updatePromises).then(() => {
+        this._snackBar.open('Cập Nhật Thành Công', '', {
+        duration: 1000,
+        horizontalPosition: "end",
+        verticalPosition: "top",
+        panelClass: ['snackbar-success'],
+        });
+        window.location.reload();
+      });
+      };
+      fileReader.readAsArrayBuffer(file);
+    }
+  
+    writeExcelFile() {
+      this._KhachhangsService.ListKhachhang()
+      const worksheet: XLSX.WorkSheet = XLSX.utils.json_to_sheet(this._KhachhangsService.ListKhachhang());
+      const workbook: XLSX.WorkBook = { Sheets: { 'Sheet1': worksheet }, SheetNames: ['Sheet1'] };
+      const excelBuffer: any = XLSX.write(workbook, { bookType: 'xlsx', type: 'array' });
+      this.saveAsExcelFile(excelBuffer, 'danhsachkhachhang_'+ moment().format("DD_MM_YYYY"));
+    }
+    saveAsExcelFile(buffer: any, fileName: string) {
+      const data: Blob = new Blob([buffer], { type: 'application/octet-stream' });
+      const url: string = window.URL.createObjectURL(data);
+      const link: HTMLAnchorElement = document.createElement('a');
+      link.href = url;
+      link.download = `${fileName}.xlsx`;
+      link.click();
+      window.URL.revokeObjectURL(url);
+      link.remove();
+    }
 }
