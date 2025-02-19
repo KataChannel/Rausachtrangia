ed1bb73ea5d6c68e0e1026bbe1a3ebfe191930f9
chikiet88 <chikiet88@gmail.com>
2025-02-19 10:11:40 +0700

    update

--- apps/beshop/src/app/donncc/donncc.module.ts
+++ apps/beshop/src/app/donncc/donncc.module.ts
@@ -3,12 +3,10 @@ import { DonnccService } from './donncc.service';
 import { DonnccController } from './donncc.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { DonnccEntity } from './entities/donncc.entity';
-import { DonnccitemEntity } from './entities/donnccitem.entity';
-import { DonnccitemService } from './entities/donnccitem.service';
   @Module({
-    imports: [TypeOrmModule.forFeature([DonnccEntity,DonnccitemEntity])],
+    imports: [TypeOrmModule.forFeature([DonnccEntity])],
     controllers: [DonnccController],
-    providers: [DonnccService,DonnccitemService],
-    exports:[DonnccService,DonnccitemService]
+    providers: [DonnccService],
+    exports:[DonnccService]
   })
   export class DonnccModule {}
